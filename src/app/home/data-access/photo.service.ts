b2da8a89b38afdbbf524a4a18fad82aeffe007c2
Zach <74432623+zandress@users.noreply.github.com>
2022-12-29 14:29:47 -0700

    modifying the takePhoto method to use Filesystem

--- src/app/home/data-access/photo.service.ts
+++ src/app/home/data-access/photo.service.ts
@@ -5,6 +5,8 @@ import {
   CameraSource,
   ImageOptions,
 } from '@capacitor/camera';
+import { Capacitor } from '@capacitor/core';
+import { Directory, Filesystem } from '@capacitor/filesystem';
 import { Platform } from '@ionic/angular';
 import { BehaviorSubject } from 'rxjs';
 import { Photo } from 'src/app/shared/interfaces/photo';
@@ -50,12 +52,24 @@ export class PhotoService {
       // Call the camera plugin
       const photo = await Camera.getPhoto(options);
 
-      if (photo.path) {
-        this.addPhoto(Date.now().toString(), photo.path);
+      const uniqueName = Date.now().toString();
+
+      if (this.platform.is('capacitor') && photo.path) {
+        const photoOnFileSystem = await Filesystem.readFile({
+          path: photo.path,
+        });
+
+        const fileName = uniqueName + '.jpeg';
+        const permanentFile = await Filesystem.writeFile({
+          data: photoOnFileSystem.data,
+          path: fileName,
+          directory: Directory.Data,
+        });
+
+        this.addPhoto(fileName, Capacitor.convertFileSrc(permanentFile.uri));
       } else if (photo.dataUrl) {
-        this.addPhoto(Date.now().toString(), photo.dataUrl);
+        this.addPhoto(uniqueName, photo.dataUrl);
       }
-      
     } catch (err) {
       console.log(err);
       throw new Error('Could not save photo');
