b775dd0a87c5dfb41076ea51d537eec015180e64
Zach <74432623+zandress@users.noreply.github.com>
2022-12-29 16:13:07 -0700

    view model stream

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,8 @@ import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
-import { map } from 'rxjs';
+import { hasShadowDom } from '@ionic/core/dist/types/utils/helpers';
+import { combineLatest, map } from 'rxjs';
 import { PhotoService } from './data-access/photo.service';
 import { PhotoListComponentModule } from './ui/photo-list.component';
 
@@ -39,6 +40,16 @@ export class HomeComponent {
         ),
       }))
     )
+  );
+
+  vm$ = combineLatest([
+    this.photos$,
+    this.photoService.hasTakenPhotoToday$,
+  ]).pipe(
+    map(([photos, hasTakenPhotoToday]) => ({
+      photos,
+      hasTakenPhotoToday,
+    }))
   );
 
   constructor(
