b38430d09b6362b266d8c66564a7097004fd3d50
Zach <74432623+zandress@users.noreply.github.com>
2022-12-29 13:03:42 -0700

    adding camera methods

--- src/app/home/data-access/photo.service.ts
+++ src/app/home/data-access/photo.service.ts
@@ -1,28 +1,62 @@
-import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
-import { Photo } from "src/app/shared/interfaces/photo";
+import { Injectable } from '@angular/core';
+import {
+  Camera,
+  CameraResultType,
+  CameraSource,
+  ImageOptions,
+} from '@capacitor/camera';
+import { Platform } from '@ionic/angular';
+import { BehaviorSubject } from 'rxjs';
+import { Photo } from 'src/app/shared/interfaces/photo';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class PhotoService {
-    #photos$ = new BehaviorSubject<Photo[]>([]);
-    photos$ = this.#photos$.asObservable();
+  #photos$ = new BehaviorSubject<Photo[]>([]);
+  photos$ = this.#photos$.asObservable();
 
-    // Receive a fileName and filePath
-    private addPhoto(fileName: string, filePath: string) {
-        // Create new photo using current date
-        const newPhotos = [
-            {
-                name: fileName,
-                path: filePath,
-                dateTaken: new Date().toISOString(),
-            },
-            // Add photo into the existing photos array
-            ...this.#photos$.value,
-        ];
+  // Receive a fileName and filePath
+  private addPhoto(fileName: string, filePath: string) {
+    // Create new photo using current date
+    const newPhotos = [
+      {
+        name: fileName,
+        path: filePath,
+        dateTaken: new Date().toISOString(),
+      },
+      // Add photo into the existing photos array
+      ...this.#photos$.value,
+    ];
 
-        // Emit it on the stream
-        this.#photos$.next(newPhotos);
+    // Emit it on the stream
+    this.#photos$.next(newPhotos);
+  }
+
+  constructor(private platform: Platform) {}
+
+  async takePhoto() {
+    const options: ImageOptions = {
+      quality: 50,
+      width: 600,
+      allowEditing: false,
+      resultType: this.platform.is('capacitor')
+        ? CameraResultType.Uri
+        : CameraResultType.DataUrl,
+      source: CameraSource.Camera,
+    };
+
+    try {
+      const photo = await Camera.getPhoto(options);
+
+      if (photo.path) {
+        this.addPhoto(Date.now().toString(), photo.path);
+      } else if (photo.dataUrl) {
+        this.addPhoto(Date.now().toString(), photo.dataUrl);
+      }
+    } catch (err) {
+      console.log(err);
+      throw new Error('Could not save photo');
     }
+  }
 }
