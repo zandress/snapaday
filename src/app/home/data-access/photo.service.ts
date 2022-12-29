da11c88d49be3331a304ee65409f5d8849f0aaea
Zach <74432623+zandress@users.noreply.github.com>
2022-12-29 11:56:34 -0700

    explanation

--- src/app/home/data-access/photo.service.ts
+++ src/app/home/data-access/photo.service.ts
@@ -9,16 +9,20 @@ export class PhotoService {
     #photos$ = new BehaviorSubject<Photo[]>([]);
     photos$ = this.#photos$.asObservable();
 
+    // Receive a fileName and filePath
     private addPhoto(fileName: string, filePath: string) {
+        // Create new photo using current date
         const newPhotos = [
             {
                 name: fileName,
                 path: filePath,
                 dateTaken: new Date().toISOString(),
             },
+            // Add photo into the existing photos array
             ...this.#photos$.value,
         ];
 
+        // Emit it on the stream
         this.#photos$.next(newPhotos);
     }
 }
