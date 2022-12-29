5d1a72bbec13b2bb9c8d448a463c356fd38e8cba
Zach <74432623+zandress@users.noreply.github.com>
2022-12-29 13:26:48 -0700

    creating the photo list component

---
+++ src/app/home/ui/photo-list.component.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from "@angular/common";
+import { ChangeDetectionStrategy, Component, Input, NgModule } from "@angular/core";
+import { IonicModule } from "@ionic/angular";
+import { Photo } from "src/app/shared/interfaces/photo";
+
+@Component({
+    selector: 'app-photo-list',
+    template: `
+        <ion-list lines="none">
+            <ion-item *ngFor="let photo of photos; trackBy: trackByFn">
+                <img [src]="photo.safeResourceUrl" />
+                <ion-badge slot="end" color="light">
+                    {{ photo.dateTaken }}
+                </ion-badge>
+            </ion-item>
+        </ion-list>
+    `,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PhotoListComponent {
+    @Input() photos!: Photo[];
+
+    constructor() {}
+
+    trackByFn(index: number, photo: Photo){
+        return photo.name;
+    }
+}
+
+@NgModule({
+    imports: [CommonModule, IonicModule],
+    declarations: [PhotoListComponent],
+    exports: [PhotoListComponent],
+})
+export class PhotoListComponentModule {}
