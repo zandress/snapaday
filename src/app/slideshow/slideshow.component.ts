9fa4bae6c9da740571f570a626c05882314abea7
Zach <74432623+zandress@users.noreply.github.com>
2022-12-30 15:40:23 -0700

    creating slideshow component

---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,51 @@
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  NgModule,
+} from '@angular/core';
+import { IonicModule, ModalController } from '@ionic/angular';
+import { BehaviorSubject } from 'rxjs';
+import { Photo } from '../shared/interfaces/photo';
+
+@Component({
+  selector: 'app-slideshow',
+  template: `
+    <ion-header>
+      <ion-toolbar color="danger">
+        <ion-title>Play</ion-title>
+        <ion-buttons slot="end">
+          <ion-button (click)="modalCtrl.dismiss()">
+            <ion-icon name="close" slot="icon-only"></ion-icon>
+          </ion-button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content> </ion-content>
+  `,
+  styles: [
+    `
+      :host {
+        height: 100%;
+      }
+    `,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SlideshowComponent {
+  currentPhotos$ = new BehaviorSubject<Photo[]>([]);
+
+  constructor(protected modalCtrl: ModalController) {}
+
+  @Input() set photos(value: Photo[]) {
+    this.currentPhotos$.next([...value].reverse());
+  }
+}
+
+@NgModule({
+  declarations: [SlideshowComponent],
+  imports: [IonicModule, CommonModule],
+  exports: [SlideshowComponent],
+})
+export class SlideshowComponentModule {}
