7475c58fcac14b1f6b18e87922d9c3d785e434e3
Zach <74432623+zandress@users.noreply.github.com>
2022-12-30 17:12:13 -0700

    adding ion item sliding and the delete method call

--- src/app/home/ui/photo-list.component.ts
+++ src/app/home/ui/photo-list.component.ts
@@ -2,8 +2,10 @@ import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
+  EventEmitter,
   Input,
   NgModule,
+  Output,
 } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { Photo } from '../../shared/interfaces/photo';
@@ -13,18 +15,26 @@ import { DaysAgoPipeModule } from './days-ago.pipe';
   selector: 'app-photo-list',
   template: `
     <ion-list lines="none">
-      <ion-item *ngFor="let photo of photos; trackBy: trackByFn">
-        <img [src]="photo.safeResourceUrl" />
-        <ion-badge slot="end" color="light">
-          {{ photo.dateTaken | daysAgo }}
-        </ion-badge>
-      </ion-item>
+      <ion-item-sliding *ngFor="let photo of photos; trackBy: trackByFn">
+        <ion-item>
+          <img [src]="photo.safeResourceUrl" />
+          <ion-badge slot="end" color="light">
+            {{ photo.dateTaken | daysAgo }}
+          </ion-badge>
+        </ion-item>
+        <ion-item-options side="end">
+          <ion-item-option (click)="delete.emit(photo.name)" color="danger">
+            <ion-icon name="trash" slot="icon-only"></ion-icon>
+          </ion-item-option>
+        </ion-item-options>
+      </ion-item-sliding>
     </ion-list>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PhotoListComponent {
   @Input() photos!: Photo[];
+  @Output() delete = new EventEmitter<string>();
 
   constructor() {}
 
